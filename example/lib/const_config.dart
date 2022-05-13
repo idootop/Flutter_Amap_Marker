4b7c876d0f2e483be8108e2435b2cb609a414161
WJG <wjg12341234@163.com>
2022-05-13 08:31:31 +0800

    chore: init project

---
+++ example/lib/const_config.dart
@@ -0,0 +1,8 @@
+import 'package:amap_flutter_base/amap_flutter_base.dart';
+
+class ConstConfig {
+  static const AMapApiKey amapApiKeys =
+      AMapApiKey(androidKey: '900f72eeee0f21e435cebb0ef155582a', iosKey: '4dfdec97b7bf0b8c13e94777103015a9');
+  static const AMapPrivacyStatement amapPrivacyStatement =
+      AMapPrivacyStatement(hasContains: true, hasShow: true, hasAgree: true);
+}
